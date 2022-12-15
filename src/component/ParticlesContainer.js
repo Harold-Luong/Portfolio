dd07575d581f0a3bc13456cf0670b33bcb79ad59
ERV_LuongNguyenDucTrong <trong.luong@ever-rise.co.jp>
2022-12-15 16:40:51 +0700

    add attribute

--- src/component/ParticlesContainer.js
+++ src/component/ParticlesContainer.js
@@ -30,74 +30,178 @@ const ParticlesContainer = () => {
             },
           },
           fpsLimit: 150,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: false,
-                mode: ["push", "remove"],
-              },
-              resize: true,
-              onHover: {
-                enable: true,
-                mode: ["grab", "slow"],
-              },
-            },
-            modes: {
-              push: {
-                quantity: 4, // click tang node
-              },
-              remove: {
-                quantity: 4, // click giam node
-              },
 
-              grab: {
-                distance: 222,
-                duration: 2.5,
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 700,
               },
             },
-          },
-          particles: {
             color: {
               value: "#ffffff",
             },
-            links: {
-              color: "#ffffff",
-              distance: 180, // khoang cach cac node
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            //va cham
-            collisions: {
-              enable: false,
-            },
-            move: {
-              directions: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
               },
-              random: false,
-              speed: 1.15,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 1800,
+              polygon: {
+                nb_sides: 5,
               },
-              value: 200,
             },
             opacity: {
               value: 0.5,
-            },
-            shape: {
-              type: "circle",
+              random: false,
+              anim: {
+                enable: false,
+                speed: 0.1,
+                opacity_min: 0.1,
+                sync: false,
+              },
             },
             size: {
-              value: { min: 1, max: 5 },
+              value: 3,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 10,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
             },
           },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "grab",
+              },
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 140,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+
+          // interactivity: {
+          //   events: {
+          //     onClick: {
+          //       enable: false,
+          //       mode: ["push", "remove"],
+          //     },
+          //     resize: true,
+          //     onHover: {
+          //       enable: true,
+          //       mode: ["grab", "slow"],
+          //     },
+          //   },
+          //   modes: {
+          //     push: {
+          //       quantity: 4, // click tang node
+          //     },
+          //     remove: {
+          //       quantity: 4, // click giam node
+          //     },
+
+          //     grab: {
+          //       distance: 222,
+          //       duration: 1.5,
+          //     },
+          //   },
+          // },
+          // particles: {
+          //   color: {
+          //     value: "#ffffff",
+          //   },
+          //   links: {
+          //     color: "#ffffff",
+          //     distance: 180, // khoang cach cac node
+          //     enable: true,
+          //     opacity: 0.5,
+          //     width: 1,
+          //   },
+          //   //va cham
+          //   collisions: {
+          //     enable: false,
+          //   },
+          //   move: {
+          //     directions: "none",
+          //     enable: true,
+          //     outModes: {
+          //       default: "bounce",
+          //     },
+          //     random: false,
+          //     speed: 1.15,
+          //     straight: false,
+          //   },
+          //   number: {
+          //     density: {
+          //       enable: true,
+          //       area: 1800,
+          //     },
+          //     value: 200,
+          //   },
+          //   opacity: {
+          //     value: 0.5,
+          //   },
+          //   shape: {
+          //     type: "circle",
+          //   },
+          //   size: {
+          //     value: { min: 1, max: 5 },
+          //   },
+          // },
           detectRetina: true,
         }}
       />
